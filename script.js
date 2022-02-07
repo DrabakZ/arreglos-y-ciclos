b41a0a22dfb9682f066a8575e66bef30caab6dc1
DrabakZ <drkzh525@gmail.com>
2022-02-06 23:33:17 -0600

    se agrego el mayor

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
+var datos = [];
+
 function guardarCalificacion(){
-    alert("MENSAJE----")
+
+    var mayor= 0;
+    var mayorImprimir= 0;
+    var calificacion = document.getElementById("calificacion").value;
+    datos.push(Number.parseInt(calificacion, 10));
+
+    var tamanioDatos = datos.length;
+
+    if(tamanioDatos > 1){
+        console.log('__________tamaño mayor a 1')
+        for (let i = 0; i < tamanioDatos; i++) {
+            if(datos[i] > datos[i+1]){
+                mayor = datos[i]
+            }else{
+                mayor = datos[i+1]
+            }
+            
+            if(mayorImprimir < mayor){
+                mayorImprimir = mayor
+            }
+            
+        }
+        document.getElementById("mayor").innerHTML = mayorImprimir
+    }
+
+    if(tamanioDatos == 1){
+        console.log('__________tamaño 1')
+        document.getElementById("mayor").innerHTML = datos[0]
+    }
+    
+
+    document.getElementById("datos").innerHTML = datos.join()
+    document.getElementById("calificacion").value = "";
+    document.getElementById("calificacion").focus();
 }
+
+
+
 
 function comparar(){
     var primerDato = document.getElementById("primero").value;
@@ -15,5 +53,5 @@ function comparar(){
         alert("El mayor es: "+primerDato)
         document.getElementById("primerNumero").innerHTML = primerDato
     }
-
+    
 }
